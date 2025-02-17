8a4ff228c1ec54f923a0c292c021affa5a51ee1f
BinHyun <binhk1004@gmail.com>
2025-02-18 00:29:11 +0900

    First Commit

---
+++ BoardApplication/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BoardApplication.Models;
+
+namespace BoardApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var BoardList = new List<Boardlist>();
+            return View(BoardList);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ BoardApplication/Models/Boardlist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace BoardApplication.Models
+{
+    public class Boardlist
+    {
+        [Key]
+        public int Listid { get; set; }
+        [DisplayName("순번")]
+        public int Number { get; set; }
+        [DisplayName("제목")]
+        public string Title { get; set; }
+        [DisplayName("작성자")]
+        public string Writer { get; set; }
+        [DisplayName("작성일")]
+        public int Published_data { get; set; }
+    }
+}
